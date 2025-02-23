5e8853d8bbb0e91f54c81de23b5babefab3bf99d
Prat0487 <praras5.pr@gmail.com>
2025-02-24 01:16:05 +0530

    fixed structure and code

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {},
-    'process.version': '"v16.0.0"',
-    'process': {
-      'version': '"v16.0.0"'
-    }
+  build: {
+    outDir: 'dist'
   }
 })
